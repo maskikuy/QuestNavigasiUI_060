db2d4097c74f2dcc46a7c91187903c2a827ab26f
maskikuy <kproject305@gmail.com>
2025-11-06 12:12:14 +0700

    menambahkan fungsi button click kedalan scaffold

--- app/src/main/java/com/example/praktikum6/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6/Navigasi.kt
@@ -1,7 +1,13 @@
 package com.example.praktikum6
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
     Formulirku,
@@ -14,5 +20,19 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
 
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
 }
