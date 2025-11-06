b08f03d60635e16ecb45fa01db951ac640734bc9
maskikuy <kproject305@gmail.com>
2025-11-06 12:06:21 +0700

    menambahkan fungsi navigasi kedalam data app

--- app/src/main/java/com/example/praktikum6/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6/Navigasi.kt
@@ -1,8 +1,18 @@
 package com.example.praktikum6
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 enum class Navigasi {
     Formulirku,
 
     Detail
 }
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+
+}
