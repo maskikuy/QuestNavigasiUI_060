baaa225018ceec68b1f28ad9c308c67e95b7f115
maskikuy <kproject305@gmail.com>
2025-11-06 12:45:02 +0700

    menambahkan composable tampil data

--- app/src/main/java/com/example/praktikum6/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6/Navigasi.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.praktikum6.view.FormIsian
+import com.example.praktikum6.view.TampilData
 
 enum class Navigasi {
     Formulirku,
@@ -33,6 +36,15 @@ fun DataApp(
                     }
                 )
             }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+            }
         }
     }
 }
+
+
---
+++ app/src/main/java/com/example/praktikum6/view/TampilData.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum6.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TampilData(
+
+){}
