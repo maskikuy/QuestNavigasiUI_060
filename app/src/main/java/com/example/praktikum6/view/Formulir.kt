55a78259a5ce55f832f807d053012944c3c49938
maskikuy <kproject305@gmail.com>
2025-11-06 13:08:42 +0700

    menambahkan fungsi scaffold

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -1,12 +1,18 @@
 package com.example.praktikum6.view
 
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick: () -> Unit
 ){
-    Scaffold {  }
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar()
+        }
+    )
 }
