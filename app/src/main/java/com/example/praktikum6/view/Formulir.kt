a08c8f01ef7f2da09a743e80f3dd8dc7cd2ebab9
maskikuy <kproject305@gmail.com>
2025-11-06 13:07:58 +0700

    menambahkan fungsi Form isian di dalam formulir kt

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -1,6 +1,12 @@
 package com.example.praktikum6.view
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
-fun FormIsian(OnSubmitBtnClick: () -> Unit) {}
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+){
+    Scaffold {  }
+}
