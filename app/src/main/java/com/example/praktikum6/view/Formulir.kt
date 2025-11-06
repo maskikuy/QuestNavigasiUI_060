97ecfb58cb4cdee49a13eece1592a2676e21e48c
maskikuy <kproject305@gmail.com>
2025-11-06 13:28:15 +0700

    menambahkan column

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -3,12 +3,16 @@
 package com.example.praktikum6.view
 
 import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -28,6 +32,12 @@ fun FormIsian(
                 colors = TopAppBarDefaults.topAppBarColors
                     (colorResource(id = R.color.teal_700))
             )}
-        ){ isiRuang->
+    ) { isiRuang->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+
+        }
     }
 }
