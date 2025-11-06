1b0865c8b0b729192ee3836b5788aefca839c20e
maskikuy <kproject305@gmail.com>
2025-11-06 13:44:17 +0700

    menambahkan button kedalam form isian

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -87,8 +88,11 @@ fun FormIsian(
             )
             Spacer(modifier = Modifier.height(30.dp))
             Button(
+                modifier = Modifier.fillMaxWidth(1f),
+                onClick = OnSubmitBtnClick
+            ) {
 
-            ) { }
+            }
         }
     }
 }
